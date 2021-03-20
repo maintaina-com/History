cefe3442ed9c318bd6d8bcbf4d0ce3449e7388d1
Ralf Lang <lang@b1-systems.de>
2021-03-20 14:39:57 +0000

    Catch case with externally installed phpunit

--- test/Horde/History/bootstrap.php
+++ test/Horde/History/bootstrap.php
@@ -2,4 +2,10 @@
 if (!class_exists(\Horde_Test_Bootstrap::class)) {
     require_once 'Horde/Test/Bootstrap.php';
 }
+$composerAutoload = __DIR__ . '/../../../vendor/autoload.php';
+
+// Catch the case where we use composer but phpunit is installed externally
+if (file_exists($composerAutoload)) {
+    require_once $composerAutoload;
+}
 Horde_Test_Bootstrap::bootstrap(__DIR__);
